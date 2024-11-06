2438ee2e2f0c5e3bdce77af6b4a168cfa3eb48a3
ppy03 <snoopy131203@gmail.com>
2024-11-06 20:49:47 +0700

    tesst

---
+++ Models/Rooms.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace real_time_chat_web.Models
+{
+    public class Rooms
+    {
+        [Key]
+        public int IdRooms { get; set; }
+        public string RoomName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+
+        //[ForeignKey("CreatedBy")]
+        //public virtual ApplicationUser User { get; set; }
+    }
+}

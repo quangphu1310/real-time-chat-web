d0197dd0d6fd5e5b57791c4fa6b1324ac99238af
quangphu <trancongquangphu10@gmail.com>
2024-11-07 20:18:34 +0700

    feat: change password endpoint

---
+++ Models/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace real_time_chat_web.Models.DTO
+{
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
